a9979fa3374d1e9e7ebd42851cc44af098efd0f8
FathimthuShada <83272049+FathimthuShada@users.noreply.github.com>
2023-03-06 23:39:02 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,57 @@
+const lastName = document.getElementById("last-name");
+const firstName = document.getElementById("first-name");
+const email = document.getElementById("email");
+const oldPass = document.getElementById("old-password");
+const newPass = document.getElementById("new-password");
+const confirmNewPass = document.getElementById("confirm-new-password");
+const form = document.getElementById("form");
+const logout = document.getElementById("logout");
+
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+// set name and email in dashboard
+// username.innerText = currentUser.name;
+// email.innerText = currentUser.email;
+
+// change current password
+form.addEventListener("submit", (e)=> {
+    // e.preventDefault();
+    if(oldPass.value !== currentUser.pass) { // if old pass and current pass doesn't match
+        alert("Old password is wrong!!!");
+    } else if(newPass.value !== confirmNewPass.value) { // if new pass and confirm pass doesn't match
+        alert("New password doesn't match!!!");
+    }
+    else if(newPass.value == currentUser.pass) { // new pass can't be same as old pass
+        alert("New password can't be same as old password!!!");
+    }
+    else { // if all form input is correct change current password in currentUser
+        currentUser = {
+            "email": currentUser.email,
+            "pass": newPass.value,
+            "name": currentUser.name,
+            "token": currentUser.token,
+        }
+
+        // update the password in users also
+        let users = JSON.parse(localStorage.users);
+        users = users.map((user)=> {
+            if(user.email == currentUser.email) {
+                user.password = newPass.value;
+            }
+            return user;
+        })
+        
+        localStorage.setItem("users", JSON.stringify(users));
+
+        // update the currentUser state
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+        alert("Password changed successfully");
+    }
+})
+
+// logout
+
+logout.addEventListener("click", ()=> {
+    currentUser = null;
+    localStorage.setItem("currentUser", null);
+    window.location.href = "./login.html"; // redirect user to login page after logging out
+})
---
+++ signup.js
@@ -0,0 +1,43 @@
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+const lastName = document.getElementById("name2");
+const firstName = document.getElementById("name1");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPass = document.getElementById("confirm-password");
+const submit = document.getElementById("submit");
+const form = document.getElementById("form");
+
+// if users not found in localStorage initialize to an empty array or get the users instead
+let users = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [];
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    if(password.value !== confirmPass.value) { // If password doesn't match show an alert
+        alert("Passwords doesn't match");
+    } else if(userAlreadyExists()) { // if email is being used already show alert
+        alert("This email is already being used, use another email");
+    } else { // create a new user and redirect to login page
+        let user = {
+            "name": firstName.value,
+            "email": email.value,
+            "password": password.value,
+        }
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        window.location.href = "./login.html";
+    }
+})
+
+// check there's an user with the same email
+function userAlreadyExists() {
+    let bool = false;
+    users.forEach((user)=> {
+        if(user.email == email.value) {
+            bool = true;
+        }
+    })
+    
+    return bool
+}
